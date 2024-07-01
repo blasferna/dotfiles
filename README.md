e2d2f8c199541dd6756230f9b056894fe959b93a
blasferna <blasferna@gmail.com>
2024-07-01 16:39:23 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # dotfiles
-My dotfiles
+
+
+## Requirements
+
+1. Install GNU Stow
+
+```bash
+sudo apt install stow
+```
+
+2. Clone the repository and run stow
+
+```bash
+cd ~
+git clone  https://github.com/blasferna/dotfiles.git
+cd dotfiles
+stow nvim
+```
+
---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	opts = {
+		transparent = false, -- change to true for transparency, it doesn't work now.
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+		--- You can override specific color groups to use other groups or a hex color
+		--- function will be called with a ColorScheme table
+		---@param colors ColorScheme
+		on_colors = function(colors) end,
+
+		--- You can override specific highlights to use other groups or a hex color
+		--- function will be called with a Highlights and ColorScheme table
+		---@param highlights Highlights
+		---@param colors ColorScheme
+		on_highlights = function(highlights, colors) end,
+	},
+	config = function(_, opts)
+		vim.cmd("colorscheme tokyonight")
+
+		-- I don't sure about the next lines.
+		local tokyonight = require("tokyonight")
+		tokyonight.setup(opts)
+		tokyonight.load()
+	end,
+}
