6b833fa539af2b731258de50d400d7fc564512cb
blasferna <blasferna@gmail.com>
2024-07-03 16:34:27 -0400

    Add outline plugin with lazy loading

---
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -0,0 +1,7 @@
+return {
+	"hedyhli/outline.nvim",
+	lazy = true,
+	cmd = { "Outline", "OutlineOpen" },
+	keys = { { "<leader>co", "<cmd>Outline<CR>", desc = "Toggle outline symbols" } },
+	opts = {},
+}
