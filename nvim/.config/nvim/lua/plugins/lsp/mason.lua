9f2991fc344e0174eb412c23863437c183921a62
blasferna <blasferna@gmail.com>
2024-12-17 14:18:56 -0300

    Added ts_ls

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,6 +1,6 @@
 return {
 	"williamboman/mason.nvim",
-	commit = "0950b15060067f752fde13a779a994f59516ce3d",
+	-- commit = "0950b15060067f752fde13a779a994f59516ce3d",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -28,6 +28,7 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
+                "ts_ls",
 				"html",
 				"cssls",
 				"tailwindcss",
