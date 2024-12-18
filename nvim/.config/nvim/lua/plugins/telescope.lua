bcb734a8ee96e454684b9241ae25c553c3d518d8
blasferna <blasferna@gmail.com>
2024-12-18 11:40:05 -0300

    Added git status as telescope option

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -42,6 +42,7 @@ return {
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find git files in cwd" })
+        keymap.set("n", "<leader>fn", "<cmd>Telescope git_status<cr>", { desc = "List git new and modified files" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set(
 			"n",
