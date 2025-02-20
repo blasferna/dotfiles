ba57bc501e77e9d1089ebe866f935c7f0b4d1411
blasferna <blasferna@gmail.com>
2025-02-20 14:22:19 -0300

    Improve html and htmldjango experience

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -52,3 +52,13 @@ api.nvim_create_autocmd("TermOpen", {
     vim.wo.relativenumber = false
   end,
 })
+
+-- Html
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"html", "htmldjango"},
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+  end
+})
