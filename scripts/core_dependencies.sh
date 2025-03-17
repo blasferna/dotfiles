7a2f9cca4c0ec12af1861f78a87c75deb2e1cb9d
blasferna <blasferna@gmail.com>
2025-03-17 09:28:57 -0300

    feat(scripts): add clipboard alias for xclip

--- scripts/core_dependencies.sh
+++ scripts/core_dependencies.sh
@@ -6,3 +6,6 @@ sudo apt install stow
 # Core dependencies
 sudo apt-get install build-essential
 sudo sudo apt install xclip
+
+echo "alias clip='xclip -selection clipboard'" >> ~/.bashrc
+source ~/.bashrc
